00b7911b4d791ef700460868d39a8b54be09c361
Vlad Shubin <shubin.va@phystech.edu>
2023-05-21 22:35:14 +0300

    Prepare for app

--- AnyNews/settings.py
+++ AnyNews/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'different_news.apps.DifferentNewsConfig',
+    'django_apscheduler',
 ]
 
 MIDDLEWARE = [
